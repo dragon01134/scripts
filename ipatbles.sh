cdd8a47c96c94b46ba98e7a5e4156b2ce4c658ff
Jitendra Kumar Mahapatra <jeet01134@gmail.com>
2017-04-05 00:46:41 +0530

    Added comments and facility to change interfaces

--- ipatbles.sh
+++ ipatbles.sh
@@ -1,8 +1,15 @@
+#Change below out interface
+OUT_IFACE=wlan0
+
+#Change below in interfaces
+IN_IFACE=eth0
+
+#flusing all iptables
 iptables --flush
 iptables --table nat --flush
 iptables --table nat --delete-chain
 
-iptables --table nat --append POSTROUTING --out-interface wlan0 -j MASQUERADE
-iptables --append FORWARD --in-interface eth0 -j ACCEPT
+iptables --table nat --append POSTROUTING --out-interface $OUT_IFACE -j MASQUERADE
+iptables --append FORWARD --in-interface $IN_IFACE -j ACCEPT
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
